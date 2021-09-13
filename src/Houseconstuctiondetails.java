1b5ed62eec9078fd72df83fc685290ec487f0c75
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-13 19:05:07 +0530

    Third house update

--- src/Houseconstuctiondetails.java
+++ src/Houseconstuctiondetails.java
@@ -3,5 +3,6 @@ public class Houseconstuctiondetails {
         System.out.println("Welcome to House construction");
         System.out.println("We will start constucting the house in details");
         System.out.println("We will start working soon");
+        System.out.println("Materials will arriving soon");
     }
 }
