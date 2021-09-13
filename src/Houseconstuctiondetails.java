d10d2f21c20264a6c77e8fae753115579c1723e7
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-13 18:51:02 +0530

    First house update

---
+++ src/Houseconstuctiondetails.java
@@ -0,0 +1,6 @@
+public class Houseconstuctiondetails {
+    public static void main(String[] args) {
+        System.out.println("Welcome to House construction");
+        System.out.println("We will start constucting the house in details");
+    }
+}
