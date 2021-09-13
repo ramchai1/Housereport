91e08c6ef79c0efd43590e823f84a03d19a91c0f
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-13 18:53:33 +0530

    Second house update

--- src/Houseconstuctiondetails.java
+++ src/Houseconstuctiondetails.java
@@ -2,5 +2,6 @@ public class Houseconstuctiondetails {
     public static void main(String[] args) {
         System.out.println("Welcome to House construction");
         System.out.println("We will start constucting the house in details");
+        System.out.println("We will start working soon");
     }
 }
